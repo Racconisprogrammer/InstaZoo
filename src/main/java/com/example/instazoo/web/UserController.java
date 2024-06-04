fb608a7ab18ca6acb003d55cdd2df736ce0c6d96
Racconisprogrammer <wedsdsa22@gmail.com>
2024-06-04 18:57:29 +0300

    completed project

---
+++ src/main/java/com/example/instazoo/web/UserController.java
@@ -0,0 +1,55 @@
+package com.example.instazoo.web;
+
+
+import com.example.instazoo.dto.UserDTO;
+import com.example.instazoo.entity.User;
+import com.example.instazoo.facade.UserFacade;
+import com.example.instazoo.service.UserService;
+import com.example.instazoo.validations.ResponseErrorValidation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("api/user")
+@CrossOrigin
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final UserFacade userFacade;
+    private final ResponseErrorValidation responseErrorValidation;
+
+    @GetMapping("/")
+    public ResponseEntity<UserDTO> getCurrentUser(Principal principal) {
+        User user = userService.getCurrentUser(principal);
+        UserDTO userDTO = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserProfile(@PathVariable("userId") String userId) {
+        User user = userService.getUserById(Long.parseLong(userId));
+        UserDTO userDTO = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Object> updateUser(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult, Principal principal) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+
+        User user = userService.updateUser(userDTO, principal);
+
+        UserDTO userUpdated = userFacade.userToUserDTO(user);
+        return new ResponseEntity<>(userUpdated, HttpStatus.OK);
+    }
+}
